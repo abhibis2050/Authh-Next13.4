f721a0f2301897da824d43a27f80ead33b782f33
drivadosayan <techsupport1@drivado.com>
2023-07-11 18:45:45 +0530

    middlewares

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import Link from 'next/link'
 import { toast } from 'react-toastify'
@@ -8,6 +8,7 @@ import { useRouter } from 'next/navigation'
 
 const ProfilePage = () => {
   const router = useRouter()
+  const [data,setData] =useState("nothing")
   const logout=async()=>{
     try {
       const response = await axios.get("/api/users/logout")
@@ -23,13 +24,22 @@ const ProfilePage = () => {
     }
   }
 
+  const getUserDetails = async()=>{
+    const response = await  axios.get("/api/users/me")
+    console.log(response?.data)
+    setData(response.data.data._id)
+  }
 
   return (
     <div>
         <h1>Profile</h1>
         <hr/>
         <p>Profile page</p>
-
+      <h2>{data==="nothing"?"Nothing":
+      <Link href={`/profile/${data}`}>
+        {data}
+      </Link>}</h2>
+        <button className='border border-black bg-orange-500 py-2 px-4 my-4 ' onClick={getUserDetails}>Get User Details</button>
         <button className='border border-black bg-yellow-300 py-2 px-4 my-4 ' onClick={logout}>Logout</button>
     </div>
   )
